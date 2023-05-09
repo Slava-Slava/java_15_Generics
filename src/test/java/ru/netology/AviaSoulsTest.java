5af7d9bc0f2f06c9b7f00a5b08638c45ab828a5c
Slava Vasiliev <sb-fantom@yandex.ru>
2023-05-09 13:05:57 +0300

    change1 README.md

--- src/test/java/ru/netology/AviaSoulsTest.java
+++ src/test/java/ru/netology/AviaSoulsTest.java
@@ -11,7 +11,9 @@ public class AviaSoulsTest {
     Ticket ticket3 = new Ticket("F.Kennedy", "NY LaGuardia", 1_000, 11_30, 14_45);//195 min
     Ticket ticket4 = new Ticket("F.Kennedy", "NY LaGuardia", 3_000, 11_30, 14_45);//195 min
     Ticket ticket5 = new Ticket("F.Kennedy", "NY LaGuardia", 800, 11_30, 12_00);//30 min
-
+    Ticket ticket6 = new Ticket("Newark Liberty", "Washington Dulles",1_000,11_29,12_00);
+    Ticket ticket7 = new Ticket("Newark Liberty", "Washington Dulles",900,11_19,13_00);
+    Ticket ticket8 = new Ticket("Newark Liberty", "Ted Stevens Anchorage",2_150,11_00,17_00);
     TicketTimeComparator timeComparator = new TicketTimeComparator();
 
     @Test
@@ -43,6 +45,9 @@ public class AviaSoulsTest {
         souls.add(ticket3);
         souls.add(ticket4);
         souls.add(ticket5);
+        souls.add(ticket6);
+        souls.add(ticket7);
+        souls.add(ticket8);
         Ticket[] expected = {ticket5, ticket1, ticket3, ticket2, ticket4};
         Ticket[] actual = souls.search("F.Kennedy", "NY LaGuardia");
         Assertions.assertArrayEquals(expected, actual);
@@ -77,8 +82,56 @@ public class AviaSoulsTest {
         souls.add(ticket3);
         souls.add(ticket4);
         souls.add(ticket5);
+        souls.add(ticket6);
+        souls.add(ticket7);
+        souls.add(ticket8);
         Ticket[] expected = {ticket1, ticket5, ticket2, ticket3, ticket4};
         Ticket[] actual = souls.searchAndSortBy("F.Kennedy", "NY LaGuardia", timeComparator);
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    void  testMultipleTicketSearch() {
+        AviaSouls souls = new AviaSouls();
+        souls.add(ticket1);
+        souls.add(ticket2);
+        souls.add(ticket3);
+        souls.add(ticket4);
+        souls.add(ticket5);
+        souls.add(ticket6);
+        souls.add(ticket7);
+        souls.add(ticket8);
+        Ticket[] expected = {ticket7,ticket6};
+        Ticket[] actual = souls.search("Newark Liberty", "Washington Dulles");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    void  testOneTicketSearch() {
+        AviaSouls souls = new AviaSouls();
+        souls.add(ticket1);
+        souls.add(ticket2);
+        souls.add(ticket3);
+        souls.add(ticket4);
+        souls.add(ticket5);
+        souls.add(ticket6);
+        souls.add(ticket7);
+        souls.add(ticket8);
+        Ticket[] expected = {ticket8};
+        Ticket[] actual = souls.search("Newark Liberty", "Ted Stevens Anchorage");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    void  testZeroTicketSearch() {
+        AviaSouls souls = new AviaSouls();
+        souls.add(ticket1);
+        souls.add(ticket2);
+        souls.add(ticket3);
+        souls.add(ticket4);
+        souls.add(ticket5);
+        souls.add(ticket6);
+        souls.add(ticket7);
+        souls.add(ticket8);
+        Ticket[] expected = {};
+        Ticket[] actual = souls.search("F.Kennedy", "Ted Stevens Anchorage");
         Assertions.assertArrayEquals(expected, actual);
     }
 
